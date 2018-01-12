70f28e0a4595a8316d6a0409797b518d8c84614c
ubuntu_mimus <ha4er122@icloud.com>
2018-01-12 05:46:11 +0300

    2.1 fix2

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ mv /home/box/stepic_web_project/web /home/box/
 sudo pip3 install django==1.11.9
 sudo pip3 install gunicorn
 cd /home/box/web/ask
-gunicorn -c '/home/nikita/projects/Course/stepik_web/gunic/gunicorn-django.py' ask.wsgi:application &
+gunicorn -c '/home/box/stepic_web_project/gunic/gunicorn-django.py' ask.wsgi:application &
 cd /home/box/stepic_web_project/gunic/
 gunicorn -c gunicorn-wsgi.py hello:app &
 sudo /etc/init.d/nginx restart
